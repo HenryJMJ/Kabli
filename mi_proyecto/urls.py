12442422ddf854a52b148e7de6ecae1ce6e51cca
HenryJMJ <henrymestra16@gmail.com>
2025-04-23 22:05:47 -0500

    Descripción de los cambios realizados

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -7,6 +7,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('usuarios.urls')),  # ← ¡esta es suficiente!
 ] 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
